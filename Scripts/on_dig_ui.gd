4ee51be62fcb9345addf43465120eb1725fa6687
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-04-10 12:27:34 +0200

    Added backpack and worknig on the toggle

--- Scripts/on_dig_ui.gd
+++ Scripts/on_dig_ui.gd
@@ -1,9 +1,16 @@
 extends CanvasLayer
 
 @onready var depth_level: RichTextLabel = $Control/DepthLevel
+@onready var backpack: Panel = $Control/Backpack
+@onready var backpack_collapser: Button = $Control/Backpack/BackpackCollapser
 
 func _ready() -> void:
 	GM.curUI = self
+
+func _backpack(toggled: bool) -> void:
+	toggled = !toggled
+	if toggled:
+		backpack.global_position.x = 0
 
 func set_depth_max_label() -> void:
 	depth_level.text = str("Max Depth: %s" % GM.maxY)
