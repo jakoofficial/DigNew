487e09d53c9f57c9809734f783e719af3b6d841f
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-05-21 14:12:17 +0200

    Added credits and a bit of styling

--- Scripts/UI/credits.gd
+++ Scripts/UI/credits.gd
@@ -1,16 +1,25 @@
 extends Control
+@onready var v_box_container: VBoxContainer = $CreditsPanel/VBoxContainer
+
 const CREDIT_ITEM = preload("uid://lxwjqqp8u2ok")
 
 var artistName: String
 var artTitle: String
 
 var credits: Dictionary[String, Array] = {
-	"TomWinandySFX": ["Block Breack Sounds"],
-	"mcanton": ["Scene Manager"],
+	"Sound Effects": ["TomWinandySFX"],
+	"Scene Manager Addon": ["mcanton"],
 	"BoldByte": [
 		"Programmer: Jacob (Jay)",
-		"Art: Jacob (Jay)"]
+		"Art: Jacob (Jay)"],
+	"Game Engine": ["Godot v4.6"]
 }
 
 func _ready() -> void:
+	for c in credits.keys():
+		var item = CREDIT_ITEM.instantiate()
+		item.get_child(0).text = c
+		for i in credits[c]:
+			item.get_child(1).text += str("%s\n" % i)
+		v_box_container.add_child(item)
 	pass
