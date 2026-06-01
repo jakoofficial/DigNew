9bc5755a39dc3b64042809c11bf32a6380947b21
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-06-01 08:30:30 +0200

    Cleaned up branch

--- Scripts/Helpers/FancyKeyObj.gd
+++
@@ -1,7 +0,0 @@
-class_name FancyKeyObj
-extends Resource
-
-@export var btn: int = -1
-@export var type: FKS.InputType = FKS.InputType.Keyboard
-@export var deviceIdx: int = 0
-@export var keycode: String = ""
--- Scripts/LevelSelect/level_select.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-@onready var level_container: FlowContainer = $LevelContainer
-@onready var back: TextureButton = $Back
-@onready var balance_label: RichTextLabel = $PlayerBalance/HBoxContainer/BalanceLabel
-
-func _ready() -> void:
-	back.connect("pressed", GM.load_scene.bind(GM.Scenes.SKILLTREE))
-	balance_label.text = str("%s" % PS._PBalance)
