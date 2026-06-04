9b6ddf3c72778aa284c9fe49a889957654772b57
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-06-04 08:55:14 +0200

    Added new End game button on level select. activates after 100% the game

--- Scripts/LevelSelect/level_select.gd
+++ Scripts/LevelSelect/level_select.gd
@@ -2,7 +2,26 @@ extends Control
 @onready var level_container: FlowContainer = $LevelContainer
 @onready var back: TextureButton = $Back
 @onready var balance_label: RichTextLabel = $PlayerBalance/HBoxContainer/BalanceLabel
+@onready var end_btn: TextureButton = $EndBtn
 
+var allCollected: bool = true
 func _ready() -> void:
 	back.connect("pressed", GM.load_scene.bind(GM.Scenes.SKILLTREE))
+	end_btn.connect("pressed", end_btn_pressed)
 	balance_label.text = str("%s" % PS._PBalance)
+	
+	for i:ArtifactRes in ArtifactInfo.Artifacts_Copy:
+		if !i._HasBeenCollected: allCollected = false; break
+	
+	if !allCollected:
+		end_btn.disabled = true
+		$EndBtn/BG.self_modulate = Color.from_rgba8(82, 82, 82, 255)
+		$EndBtn/BG/EndBtnText.text = str("? ? ?")
+	else:
+		end_btn.disabled = false
+		$EndBtn/BG.self_modulate = Color.from_rgba8(255, 255, 255, 255)
+		$EndBtn/BG/EndBtnText.text = str("End?")
+
+
+func end_btn_pressed() -> void:
+	pass
